4f47fce64aa395c6d9fd5eba578842019e46289f
Jon Swallow <joncswallow@gmail.com>
2019-01-27 18:29:47 -0700

    Create video.js

---
+++ video.js
@@ -0,0 +1,21 @@
+(function()  {
+	var video= document.getElementById( 'video'),
+			vendorUrl = window.URL || window.webkitURL;
+
+			navigator.getMedia = navigator.getUserMEdia ||
+			navigator.webkitGetUserMedia ||
+			navigator.mozGetUserMedia ||
+			navigator.msGetUserMedia;
+
+			// Capture video
+			navigator.getMedia({
+				video: true,
+				audio: false,
+				}, function(stream); {
+                 video.src = vendorUrl.createObjectURL(stream);
+                 video.play();
+			    }, function(error) {
+			    	// body...
+		
+			});
+})();
