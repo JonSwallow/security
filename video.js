2dc264f0ea5b859fca707106970508b7887f4e70
Jon Swallow <joncswallow@gmail.com>
2019-01-27 19:22:14 -0700

    Update video.js

--- video.js
+++ video.js
@@ -12,7 +12,7 @@
 				video: true,
 				audio: false
     }, function(stream) {
-                 video.src = vendorUrl.createObjectURL(stream);
+                  video.srcObject = stream;
                  video.play();
     }, function(error) {
 			    	// body...
